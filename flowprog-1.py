b3f9e0af542910ffa18b4184813f9934ab70882f
Hasansalem88 <mahmoud.elsakoot@gmail.com>
2025-05-05 11:57:15 +0300

    Update flowprog-1.py

--- flowprog-1.py
+++ flowprog-1.py
@@ -27,8 +27,8 @@ credentials = to_dict(st.secrets["credentials"])
 # Initialize the authenticator
 authenticator = stauth.Authenticate(
     credentials,
-    "auth_token",  # replace with your cookie name
-    "KEuQXEyCIt1AgyIFd5LQi85XmAGB8fsN8i2GdeN9DHQ",  # replace with your signature key
+    "admin",  # replace with your cookie name
+    "$2b$12$8ajX8oB.VpxK9ytwgkl./uu1RDGi84bLyEEAPBceFLk1yDWdWMFlu",  # replace with your signature key
     cookie_expiry_days=1
 )
 
